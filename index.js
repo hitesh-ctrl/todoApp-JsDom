347a78e22636cc6393833b0137cf9d93ebbb5163
hitesh-ctrl <71893561+hitesh-ctrl@users.noreply.github.com>
2025-06-24 16:35:54 +0530

    fix:empty todos

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ let div=document.createElement("div")
 div.setAttribute("id","todos")
 document.querySelector("body").append(div)
 const add=()=>{
-    let inp=document.querySelector("input").value
-    todos.push(inp)
-    render()
+    
+    let inp=document.querySelector("input").value.trim()
+    if (inp !== "") { 
+        todos.push(inp);
+        render();
+    }
 }
 
 const del=(i)=>{
@@ -14,45 +17,45 @@ const del=(i)=>{
 }
 
 const render=()=>{
-    document.getElementById("todos").innerHTML=""
-    for(let i=0;i<todos.length;i++){
-        let el=document.createElement("div");
-        
-        let spel=document.createElement("span")
-        spel.innerText=todos[i]
-
-        let btn=document.createElement("button")
-        btn.innerText="delete"
-        btn.onclick=()=>{del(i)}
-        
-        
-        let editBtn=document.createElement("button")
-        editBtn.innerText="Edit"
-        
-        editBtn.onclick=()=>{
-            let editInp=document.createElement("input")
-            editInp.type="text"
-            editInp.value=todos[i]
-
-            let saveBtn=document.createElement("button")
-            saveBtn.innerText="Save"
-            saveBtn.onclick=()=>{
-                let updatedtxt=editInp.value.trim();
-                if(updatedtxt != ""){
-                    todos[i]=updatedtxt;
-                
-                }
-                render()
-            }
+    
+        document.getElementById("todos").innerHTML=""
+        for(let i=0;i<todos.length;i++){
+            let el=document.createElement("div");
             
-            el.replaceChild(editInp, spel);
-            el.replaceChild(saveBtn,editBtn)
-        }
-        div.append(el)
-        el.append(spel,btn,editBtn)
-        
-       
-    }
-    document.querySelector("input").value=""
+            let spel=document.createElement("span")
+            spel.innerText=todos[i]
 
-}
+            let btn=document.createElement("button")
+            btn.innerText="delete"
+            btn.onclick=()=>{del(i)}
+            
+            
+            let editBtn=document.createElement("button")
+            editBtn.innerText="Edit"
+            
+            editBtn.onclick=()=>{
+                let editInp=document.createElement("input")
+                editInp.type="text"
+                editInp.value=todos[i]
+
+                let saveBtn=document.createElement("button")
+                saveBtn.innerText="Save"
+                saveBtn.onclick=()=>{
+                    let updatedtxt=editInp.value.trim();
+                    if(updatedtxt != ""){
+                        todos[i]=updatedtxt;
+                    
+                    }
+                    render()
+                }
+                
+                el.replaceChild(editInp, spel);
+                el.replaceChild(saveBtn,editBtn)
+            }
+            div.append(el)
+            el.append(spel,btn,editBtn)
+            
+        
+        }
+        document.querySelector("input").value=""
+    }
